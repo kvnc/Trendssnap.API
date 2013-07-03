cf90f30cb166acdacf676f01ffacc099ae40299a
Kevin Cieszkowski <kevinjc@gmx.com>
2013-07-02 22:40:52 -0700

    Initial Trendssnap.API check-in

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TrendssnapApi::Application.config.secret_key_base = '52d58013d506e0a2f7f555f21d2f4dbf2252eceb79cdc917d6f90a47f10cdea848af58ceadd65dea45181cdd2492edf7fea794981fdb3ecbd1bfa3649731a73e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TrendssnapApi::Application.config.session_store :cookie_store, key: '_Trendssnap_API_session'
